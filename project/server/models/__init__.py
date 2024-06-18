676ea7fcb5e83832950db46f3da2cafab5fb53b9
Dima Dzundza <dzundzan@gmail.com>
2024-06-19 02:36:56 +0300

    move database tables

---
+++ project/server/models/__init__.py
@@ -0,0 +1,2 @@
+from .users import User
+from .black_list_token import BlacklistToken
--- project/server/models/users.py
+++ project/server/models/users.py
@@ -1,10 +1,9 @@
-# project/server/models.py
-
-
-import jwt
 import datetime
 
 from project.server import app, db, bcrypt
+import jwt
+
+from .black_list_token import BlacklistToken
 
 
 class User(db.Model):
@@ -63,30 +62,3 @@ class User(db.Model):
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again.'
-
-
-class BlacklistToken(db.Model):
-    """
-    Token Model for storing JWT tokens
-    """
-    __tablename__ = 'blacklist_tokens'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    token = db.Column(db.String(500), unique=True, nullable=False)
-    blacklisted_on = db.Column(db.DateTime, nullable=False)
-
-    def __init__(self, token):
-        self.token = token
-        self.blacklisted_on = datetime.datetime.now()
-
-    def __repr__(self):
-        return '<id: token: {}'.format(self.token)
-
-    @staticmethod
-    def check_blacklist(auth_token):
-        # check whether auth token has been blacklisted
-        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
-        if res:
-            return True
-        else:
-            return False
